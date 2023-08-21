3a47f57f363a27aedb14074a374f78d3399b1c6f
Thomas Almeida <thomasalmeida.dev@gmail.com>
2023-08-21 15:07:36 -0300

    added find by date range in transaction repository

--- app/repositories/transaction_repository.rb
+++ app/repositories/transaction_repository.rb
@@ -2,4 +2,10 @@ class TransactionRepository
   def self.create(sender:, receiver:, amount_in_cents:)
     Transaction.create(debit_account: sender, credit_account: receiver, amount_in_cents:)
   end
+
+  def self.find_by_date_range(account_id:, start_date:, end_date:)
+    Transaction.where('debit_account_id = ? OR credit_account_id = ?', account_id, account_id)
+               .where(created_at: start_date..end_date)
+               .order(:created_at)
+  end
 end
--- spec/repositories/transaction_repository_spec.rb
+++ spec/repositories/transaction_repository_spec.rb
@@ -17,4 +17,17 @@ RSpec.describe TransactionRepository do
         .to change(Transaction, :count).by(1)
     end
   end
+
+  describe '.find_by_date_range' do
+    let(:account) { FactoryBot.create(:account) }
+    let(:start_date) { Time.zone.today.beginning_of_month }
+    let(:end_date) { Time.zone.today.end_of_month }
+    let!(:transaction1) { FactoryBot.create(:transaction, debit_account: account, created_at: start_date + 1.day) }
+    let!(:transaction2) { FactoryBot.create(:transaction, credit_account: account, created_at: start_date + 2.days) }
+
+    it 'returns transactions within the specified date range for an account' do
+      result = described_class.find_by_date_range(account_id: account.id, start_date:, end_date:)
+      expect(result).to match_array([transaction1, transaction2])
+    end
+  end
 end
