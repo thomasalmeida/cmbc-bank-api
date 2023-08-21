31e272e6b628808ba6eda46787b57c4fe50607fc
Thomas Almeida <thomasalmeida.dev@gmail.com>
2023-08-21 14:51:36 -0300

    added missing response helper in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
   include AuthenticatedController
+  include ResponseHelper
+
   def not_found
     render json: { error: 'Not found' }, status: :not_found
   end
